2dfd9ce551aed466e1b9e6615d58e4f9e8fcb847
manishjamod065-png <manishjamod065@gmail.com>
2026-02-26 14:28:22 +0530

    spiral_print

---
+++ lecture 2/spiral_print.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int r,c;
+    cin>>r>>c;
+    vector<vector<int>> arr(r,vector<int>(c));
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int left=0,right=c-1,top=0,bottom=r-1;
+    while(left<=right and top<=bottom){
+        for(int i=left;i<=right;i++  ){
+            cout<<arr[top][i]<<" ";
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+            cout<<arr[i][right]<<" ";
+
+        }
+        bottom--;  
+    }
+    if(left<=right){
+        for(int i=bottom;i>=top;i--){
+            cout<<arr[i][left]<<" ";
+        }
+    }
+    left++;
+
+        }
+    
+
